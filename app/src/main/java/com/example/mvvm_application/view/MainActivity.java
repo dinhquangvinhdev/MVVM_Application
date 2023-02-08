0c197cab8ebc3ef863ff8d2eec3a19fbe739e3b1
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-02-08 13:33:22 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/mvvm_application/view/MainActivity.java
+++ app/src/main/java/com/example/mvvm_application/view/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.mvvm_application.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.example.mvvm_application.R;
+import com.example.mvvm_application.databinding.ActivityMainBinding;
+import com.example.mvvm_application.model.Note;
+import com.example.mvvm_application.vm.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private NoteViewModel noteVM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initView();
+        initVM();
     }
+
+    private void initView() {
+
+    }
+
+    private void initVM() {
+        // Create a ViewModel the first time the system calls an activity's onCreate() method.
+        // Re-created activities receive the same NoteViewModel instance created by the first activity.
+        noteVM = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteVM.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, notes.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
 }
